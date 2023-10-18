bad6a497989c32bd66043fbfd924f69f9b8f3a15
Angel Adell <angeldeniseadell@gmail.com>
2023-10-18 16:24:36 -0400

    add the first test and solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -13,3 +13,8 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+describe("sayHello", function () {
+    it('should bbe a defined function', function () {
+        expect(typeof sayHello).toBe('function');
+    });
+});
